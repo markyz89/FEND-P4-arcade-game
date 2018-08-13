487fb631b0a82e09e4da4b03cfdf80612d3ed9b4
Mark Simpson <markyz1989@gmail.com>
2018-08-13 09:53:37 +0100

    Completed app

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
-frontend-nanodegree-arcade-game
-===============================
+# Arcade Game Classic
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+This is a version of the classic arcade game 'Frogger' created as part of the Udacity Front End Nanodegree programme. The game utilises HTML, CSS and Javascript to create an object orientated game with collisions.
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+
+
+## Instructions
+
+
+Load the game by running the index.html file in a browser.
+
+Your player is the character located at the bottom of the game area. To move your player, you can use the arrow keys on your keyboard.
+
+The object of the game is to get the character from the grass, across the three rows of tiles and into the water. Unfortunately, a horde of cockroaches patrol these tiles, and coming into contact with any of them will send your character back to the starting point. You must avoid the cockroaches if you want to reach the water!
+
+Once you reach the water, the game restarts with your character back in the starting position, ready to play again.
+
+
+
+
+## Contributing 
+
+This is an assessed project for the Udacity Nanodegree programme so I will most likely not be accepting pull requests as that would probably be considered cheating.
+
+## Acknowledgements
+
+The game's engine.js file and resources.js file were built by the good people at Udacity, making this project possible.
--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    this.x += (this.speed*dt);
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +31,185 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+        // no need for speed as movement is not automatic
+var Player = function (x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png'; 
+}
+
+Player.prototype.update = function (dt) {
+    // basically just copied enemy function here
+}
+
+
+// this prototype function handles the player movement
+Player.prototype.handleInput = function (keyPress) {
+     if (keyPress == 'left' && this.x > 0) {
+        // console.log("pressed left!");
+        this.x -=100;
+    }
+
+    if (keyPress == 'right' && this.x < 400) {
+        // console.log("pressed right!");
+        this.x +=100;
+    }
+
+    if (keyPress == 'up' && this.y > -20) {
+        // console.log("pressed up!");
+        this.y -=80;
+    }
+
+    if (keyPress == 'down' && this.y < 380) {
+        // console.log("pressed down!");
+        this.y +=80;
+    }
+
+
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var score = 1;
+var winningPosition = false;
+
+function increaseScore () {
+    var scoreboard = document.getElementById('scoreboard');
+    if (winningPosition == true) {
+        score += 1;    
+    }
+} 
+
+
+function success () {
+    winningPosition = true;
+    var modal = document.getElementById('success-modal');
+    scoreboard.innerHTML = "Score: "+score;
+    modal.style.display = 'block';
+    setTimeout( function(){
+        modal.style.display = 'none';
+    }, 1500);
+}
+
+
+
+// create a resetPlayer() method
+
+// moves the player back to the start when reaching the water after a short delay.
+Player.prototype.resetPlayer = function() {
+    if (this.y < 55) {
+        console.log('made the water!')
+
+       success();
+       setTimeout(function(){
+            player.y = 380;
+            // insert score functionality here in udacious version
+            increaseScore();
+        }, 750);
+       
+    } 
+};
+
+
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+// only need one player object
+var player = new Player (200, 380);
+// create empty array to be accessed by createEnemies() function
+var allEnemies = [];
+
+
+// the function that generates the intial enemies on screen
+function createEnemies () {
+    console.log('creating enemies')
+    for (var i = 0; i < 5; i++) {
+        var enemy = new Enemy(enemySpawn(), enemyLocation(), enemySpeed());
+        allEnemies.push(enemy);
+                
+    // console.log('bug logged');
+    }
+};
+
+createEnemies();
+    // this function checks the number of roaches on screen and add new ones once the roaches have passed through the game area
+    // the function also handles the collisions, checking for them 20 times per second
+window.setInterval(function checkRoaches() {
+    for (var k = 0; k < allEnemies.length; k++) {
+                if (allEnemies[k].x > 505) {
+                    allEnemies.splice(k, 1);
+                    var nextWave = new Enemy(enemySpawn(), enemyLocation(), enemySpeed());
+                    allEnemies.push(nextWave);
+                }
+            }
+                winningPosition = false;
+                player.resetPlayer();
+                collisions();
+                // console.log(player.y, player.x);
+
+
+  // console.log("checking...")
+}, 50);
+
+// this function determines whether a player has come into contact with the enemy. 
+function collisions () {
+     for (var i = 0; i < allEnemies.length; i++) {
+        if (allEnemies[i].y > player.y-20 && allEnemies[i].y < player.y+20 && allEnemies[i].x < player.x+60 && allEnemies[i].x > player.x-60) {
+            player.y = 380;
+            // console.log('collision occured!');
+            // life lost function
+        }
+    }
+}
+
+
+
+// this function determines the speed of the roaches. The speed is random.
+var roachSpeed;
+function enemySpeed () {
+    roachSpeed = (Math.random()*250)+100;
+    return roachSpeed; 
+
+
+}
+
+// this function determines which of the three rows the roaches should travel along.
+function enemyLocation() {
+    tier = Math.round(Math.random()*3);
+    if (tier === 3) {
+        return 225;
+    }
+    else if (tier === 2) {
+        return 140;
+    }
+    else if (tier === 1) {
+        return 55;
+    }
+    else {
+        console.log("got a zero");
+        return enemyLocation();
+    }
+}
+
+var spawn;
+
+//this sets the enemy's spawn locations, which are at random points on the x-axis to the left of the game area.
+function enemySpawn() {
+     spawn = -Math.round((Math.random()*1000)+10);
+         for (var j = 0; j < allEnemies.length; j++) {
+            if (spawn > (allEnemies[j].x - 100) && spawn < (allEnemies[j].x + 100)) {
+                enemySpawn();
+            } else {
+                return spawn;
+            }           
+         }
+     }
 
 
 // This listens for key presses and sends the keys to your
@@ -43,4 +223,7 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+    // console.log("something pressed");
 });
+
+
